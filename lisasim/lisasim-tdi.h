947425890299b5c683c347d321552ae649a33419
Michele Vallisneri <vallis@vallis.org>
2004-02-19 21:06:04 +0000

    New 2nd-gen expressions
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@78 35f09e0b-191c-0410-822b-d9f812da8b64

--- lisasim/lisasim-tdi.h
+++ lisasim/lisasim-tdi.h
@@ -18,7 +18,15 @@ class TDI {
         virtual double alpha(double t);
         virtual double beta(double t);
         virtual double gamma(double t);
-    
+
+	virtual double alpham(double t);
+	virtual double betam(double t);
+	virtual double gammam(double t);
+
+	virtual double alpha1(double t);
+	virtual double alpha2(double t);
+	virtual double alpha3(double t);
+
         virtual double zeta(double t);
 
 	virtual double P(double t);
@@ -30,6 +38,8 @@ class TDI {
 	virtual double Zm(double t);
 
 	virtual double X1(double t);
+	virtual double X2(double t);
+	virtual double X3(double t);
 
         virtual double y(int send, int link, int recv, int ret1, int ret2, int ret3, double t) {return 0.0;};
         virtual double z(int send, int link, int recv, int ret1, int ret2, int ret3, int ret4, double t) {return 0.0;};
