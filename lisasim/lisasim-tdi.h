0d9f9ed6629c3e5a95fc82143034fe4a3e6cdd45
Michele Vallisneri <vallis@vallis.org>
2004-07-28 19:26:10 +0000

    Moved retardtime mechanism to base LISA class methods
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@120 35f09e0b-191c-0410-822b-d9f812da8b64

--- lisasim/lisasim-tdi.h
+++ lisasim/lisasim-tdi.h
@@ -29,6 +29,10 @@ class TDI {
 
         virtual double zeta(double t);
 
+	virtual double zeta1(double t);
+	virtual double zeta2(double t);
+	virtual double zeta3(double t);
+
 	virtual double P(double t);
 	virtual double E(double t);
 	virtual double U(double t);
@@ -50,40 +54,6 @@ class TDI {
 
 	virtual double y(int send, int link, int recv, int ret1, int ret2, int ret3, int ret4, int ret5, int ret6, int ret7, double t) {return 0.0;};
 	virtual double z(int send, int link, int recv, int ret1, int ret2, int ret3, int ret4, int ret5, int ret6, int ret7, int ret8, double t) {return 0.0;};
-
-};
-
-class retardtime {
- private:
-    LISA *lisa;
-
-    double it, rt;
-    double trb, tra;
-
- public:
-    retardtime(LISA *l, double t) : lisa(l), it(t), rt(t), trb(0.0), tra(0.0) {};
-    
-    double retardedtime() {
-	return rt;
-    };
-
-    void retard(int ret) {
-	if (ret != 0) {
-	    trb += lisa->armlengthbaseline(ret,rt);  
-	    tra += lisa->armlengthaccurate(ret,rt);
-
-	    rt = (it - trb) - tra;
-	}
-    };
-
-    void retard(LISA *anotherlisa, int ret) {
-	if (ret != 0) {
-	    trb += anotherlisa->armlengthbaseline(ret,rt);  
-	    tra += anotherlisa->armlengthaccurate(ret,rt);
-
-	    rt = (it - trb) - tra;
-	}
-    };
 };
 
 #endif /* _LISASIM_TDI_H_ */
