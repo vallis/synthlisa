0f44fdf35224a6e035f518febc2ad651e270d265
Michele Vallisneri <vallis@vallis.org>
2004-07-28 19:32:46 +0000

    Second version of CacheLISA, based on home-grown hashed map. Better.
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@126 35f09e0b-191c-0410-822b-d9f812da8b64

--- lisasim/lisasim-retard.h
+++ lisasim/lisasim-retard.h
@@ -1,168 +1,198 @@
 #ifndef _LISASIM_RETARD_H_
 #define _LISASIM_RETARD_H_
 
-#include <map>
-
 #include "lisasim-lisa.h"
 
-/// define retardation key
+/** Length of the cache buffer. It should be at least 2^(3 + maximum
+    number of retardations - 1). */
 
-class retardpos {
- public:
-    double time;
-    long arms;
+static const unsigned long buflength = 1024;
 
-    retardpos(double t,long a) : time(t), arms(a) {};
-};
-
-/// define weak comparison operator for retardation keys
-
-class poscomp {
- public:
-    bool operator()(const retardpos &a1,const retardpos &a2) {
-	if (a1.time != a2.time)  // maybe should use fabs
-	    return a1.time < a2.time;
-	else
-	    return a1.arms < a2.arms;
-    }
-};
-
-/// define retardation value
-
-class retardval {
- public:
-    double rt, trb, tra;
-
-    retardval(): rt(0.0), trb(0.0), tra(0.0) {};
-
-    retardval(double r,double b,double a): rt(r), trb(b), tra(a) {};
-};
-
-typedef map<retardpos,retardval,poscomp> retardmap;
+/** Cached LISA class. Much like NoisyLISA, this class works by
+    routing armlength and putp-putn calls to a "basiclisa" object that
+    is passed upon construction. However, CacheLISA interposes a layer
+    of its own making for "retard" calls: it maintains a cache of
+    retardations for the most recently accessed time, and will return
+    cached information if it has it. */
 
 class CacheLISA : public LISA {
  private:
-    double it;  // initial time
-    double rt;  // retarded time so far
+    /// Initial reference time for retardation.
+    double it;  
 
-    double trb; // baseline retardation
-    double tra; // additional accurate retardation
+    /// Retarded time so far.
+    double rt;  
 
-    long rts;   // all retardations so far
+    /// Cumulative baseline retardation.
+    double trb; 
 
+    /// Cumulative additional accurate retardation.
+    double tra; 
+
+    /** Retardation key and hashed retardation key. The key is coded
+	by expressing each retarding arm as a 3-bit signed integer,
+	and shifting to the right as retarding arms are added. The
+	hashed key is coded by doing the first retarding arm as for
+	the key, but then coding each additional retarding arm as 1 if
+	it is equal (in absolute value) to the preceding retarding
+	arm, and 0 otherwise; therefore the hashed key is shifted to
+	the right only by 1 before adding each new retarding arm. This
+	hashed coding should be unique for causal retardations
+	(corresponding to geometric paths), where it is essentially
+	equivalent to a cw-ccw coding. */
+
+    unsigned long rts, hash;
+
+    /// Keeps track of the last retardation (needed to form hash).
+    int lastarm;
+
+    /// Basic LISA object.
     LISA *basiclisa;
 
-    double buffertime, oldesttime;
+    /// Cache keys.
+    unsigned long keys[buflength];
 
-    retardmap *mymap;
+    /// Caches for it, rt, trb, and tra.
+    double its[buflength], rtis[buflength], trbs[buflength], tras[buflength];
 
  public:
-    CacheLISA(LISA *l,double bt) : basiclisa(l), buffertime(bt), oldesttime(0.0) {
-	mymap = new retardmap();
+    /// Default constructor. Sets caches and counters to zero.
+    CacheLISA(LISA *l) : basiclisa(l) {
+	for(int i=0;i<buflength;i++) {
+	    keys[i] = 0;
+	    its[i] = 0.0; rtis[i] = 0.0; trbs[i] = 0.0; tras[i] = 0.0;
+	}
+
+	newretardtime(0.0);
     }
 
-    ~CacheLISA() {
-	delete mymap;
-    }
-
+    /// Reset function. Sets caches and counters to zero, and calls
+    /// reset for basiclisa.
     void reset() {
-	mymap->clear();
+	for(int i=0;i<buflength;i++) {
+	    keys[i] = 0;
+	    its[i] = 0.0; rtis[i] = 0.0; trbs[i] = 0.0; tras[i] = 0.0;
+	}
 
-	oldesttime = 0.0;
+	newretardtime(0.0);
 
 	basiclisa->reset();
     }
 
+    // The following is all standard for "encapsulating" LISA objects.
+
+    /// Returns underlying PHYSICAL LISA object.
     LISA *thislisa() {
-	return basiclisa;
+	return basiclisa->thislisa();
     }
 
+    /// Calls underlying LISA for armlength
     double armlength(int arm, double t) {
 	return basiclisa->armlength(arm,t);
     }
 
+    /// Calls underlying LISA for armlengthbaseline
     double armlengthbaseline(int arm, double t) {
 	return basiclisa->armlengthbaseline(arm,t);
     }
 
+    /// Calls underlying LISA for armlengthaccurate
     double armlengthaccurate(int arm, double t) {
 	return basiclisa->armlengthaccurate(arm,t);
     }
 
+    /// Calls underlying LISA for putn
     void putn(Vector &n, int arm, double t) {
 	basiclisa->putn(n,arm,t);
     }
-        
+
+    /// Calls underlying LISA for putp        
     void putp(Vector &p, int craft, double t) {
 	basiclisa->putp(p,craft,t);
     }
 
+    /// Calls underlying LISA for putn[]
     void putn(double n[], int arm, double t) {
 	basiclisa->putn(n,arm,t);
     }
-        
+
+    /// Calls underlying LISA for putp[]
     void putp(double p[], int craft, double t) {
 	basiclisa->putp(p,craft,t);
     }
 
+    // The following is specific to CacheLISA.
+
+    /// Sets up counters for a new retardation.
     void newretardtime(double t) {
 	it = t; rt = t;
 	trb = 0.0; tra = 0.0;
-	rts = 0;
-
-	// clean buffer; alternative algorithm
-
-	if(t != oldesttime) {
-	    oldesttime = t;
-	    mymap->clear();
-	}
-
-	return;
-
-	// clean buffer; simplest algorithm
-
-	if(t - buffertime > oldesttime) {
-	    oldesttime = t - buffertime;
-
-	    for (retardmap::iterator iter = mymap->begin(); iter != mymap->end(); /* empty */) {
-		double thistime = (*iter).first.time;
-
-		if( thistime < oldesttime )
-		    mymap->erase(iter++); // must use postfix increment
-		else
-		    break; // was ++iter; but we are betting on ordered traversal
-	    }
-	}
+	rts = 0; hash = 0;
+	lastarm = 0;
     }
 
+    /// Returns the retarded time so far.
     double retardedtime() {
 	return rt;
     }
 
+    /// Computes a retardation. Contains the caching mechanism.
     void retard(int ret) {
-	if (ret != 0) {
+	if(ret == 0)
+	    return;
+
+	// Update the retardation key and the hash.
+	if(rts == 0) {
 	    rts = (rts << 3) | (ret > 0 ? ret : (4 | -ret));
-	    retardpos rpos(it,rts);
+	    hash = rts;
+	} else {
+	    rts = (rts << 3) | (ret > 0 ? ret : (4 | -ret));
+	    
+	    hash = hash << 1;
+	    if(abs(ret) == abs(lastarm))
+		rts = rts | 1;
+	}
+	
+	lastarm = ret;
 
-	    retardmap::iterator iter = mymap->find(rpos);
+	// We cannot handle such as a large hash. Should throw exception.
+	if(hash >= buflength) {
+	    cout << "CacheLISA: Hash out of range in retard()!" << endl;
+	    abort();
+	}
 
-	    if(iter == mymap->end()) { // nah, don't have it...
-		trb += basiclisa->armlengthbaseline(ret,rt);
-		tra += basiclisa->armlengthaccurate(ret,rt);
+	if(its[hash] == it && keys[hash] == rts) {
+	    // Got it!
+
+	    rt = rtis[hash];
+	    trb = trbs[hash]; tra = tras[hash];
+	} else {
+	    // Nah, will have to compute it.
+
+	    trb += basiclisa->armlengthbaseline(ret,rt);
+	    tra += basiclisa->armlengthaccurate(ret,rt);
 		
-		rt = (it - trb) - tra;
+	    rt = (it - trb) - tra;
+	}
 
-		retardval rval(rt,trb,tra);
+	if(its[hash] < it) {
+	    // We can replace the old value in the cache with the
+	    // newly computed value. But we don't do this if the time
+	    // is the same, but the key is different (a cache collision).
 
-		(*mymap)[rpos] = rval;
-	    } else {                // yup, got it!
-		rt  = (*iter).second.rt;
+	    its[hash] = it;
+	    keys[hash] = rts;
 
-		trb = (*iter).second.trb;
-		tra = (*iter).second.tra;
-	    }
+	    rtis[hash] = rt;
+	    trbs[hash] = trb; tras[hash] = tra;
 	}
     }
+
+    /** Computes a retardation with a different LISA geometry
+	[generally it will be basiclisa->thislisa()]. Note that
+	calling this form of retard will (probably) make the cache
+	state undefined, so we should never do it unless it's the last
+	retard call */
 
     void retard(LISA *anotherlisa,int ret) {
 	if (ret != 0) {
