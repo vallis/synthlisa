cae24dedc83389d2fdf49f75c470a067a3abc85e
Michele Vallisneri <vallis@vallis.org>
2005-07-26 14:53:21 +0000

    Minimal changes
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@195 35f09e0b-191c-0410-822b-d9f812da8b64

--- bin/recompile-here.sh
+++ bin/recompile-here.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# $Author$Revision: 1.1 $Date: 2005/07/26 14:49:30
+# $Id:
 
 python setup.py install --with-numeric=`pwd` --with-swig=/Users/vallis/Documents/synthlisa/bin/swig --prefix=`pwd`
