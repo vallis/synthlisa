70daacbeddffe2dcbc931e65456e3dff1568e0f4
Michele Vallisneri <vallis@vallis.org>
2005-11-11 02:44:28 +0000

    Added exception for invalid hash in CacheLISA::retard; fixed indentation
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@223 35f09e0b-191c-0410-822b-d9f812da8b64

--- lisasim/lisasim-retard.h
+++ lisasim/lisasim-retard.h
@@ -1,7 +1,18 @@
+/* $Id$
+ * $Date$
+ * $Author$
+ * $Revision$
+ */
+
 #ifndef _LISASIM_RETARD_H_
 #define _LISASIM_RETARD_H_
 
 #include "lisasim-lisa.h"
+#include "lisasim-except.h"
+
+#include <iostream>
+
+// ??? Might be good to move all these function definitions in a cpp file
 
 /** Length of the cache buffer. It should be at least 2^(3 + maximum
     number of retardations - 1). */
@@ -58,139 +69,130 @@ class CacheLISA : public LISA {
  public:
     /// Default constructor. Sets caches and counters to zero.
     CacheLISA(LISA *l) : basiclisa(l) {
-	for(unsigned int i=0;i<buflength;i++) {
-	    keys[i] = 0;
-	    its[i] = 0.0; rtis[i] = 0.0; trbs[i] = 0.0; tras[i] = 0.0;
-	}
+	   for(unsigned int i=0;i<buflength;i++) {
+	       keys[i] = 0;
+	       its[i] = 0.0; rtis[i] = 0.0; trbs[i] = 0.0; tras[i] = 0.0;
+	   }
 
-	newretardtime(0.0);
-    }
+	   newretardtime(0.0);
+    };
 
     /// Reset function. Sets caches and counters to zero, and calls
     /// reset for basiclisa.
     void reset() {
-	for(unsigned int i=0;i<buflength;i++) {
-	    keys[i] = 0;
-	    its[i] = 0.0; rtis[i] = 0.0; trbs[i] = 0.0; tras[i] = 0.0;
-	}
+	   for(unsigned int i=0;i<buflength;i++) {
+	       keys[i] = 0;
+	       its[i] = 0.0; rtis[i] = 0.0; trbs[i] = 0.0; tras[i] = 0.0;
+	   }
 
-	newretardtime(0.0);
+	   newretardtime(0.0);
 
-	basiclisa->reset();
-    }
+	   basiclisa->reset();
+    };
 
     // The following is all standard for "encapsulating" LISA objects.
 
     /// Returns underlying PHYSICAL LISA object.
     LISA *physlisa() {
-	return basiclisa->physlisa();
-    }
+    	return basiclisa->physlisa();
+    };
 
     /// Calls underlying LISA for armlength
     double armlength(int arm, double t) {
-	return basiclisa->armlength(arm,t);
-    }
+	   return basiclisa->armlength(arm,t);
+    };
 
     /// Calls underlying LISA for armlengthbaseline
     double armlengthbaseline(int arm, double t) {
-	return basiclisa->armlengthbaseline(arm,t);
-    }
+	   return basiclisa->armlengthbaseline(arm,t);
+    };
 
     /// Calls underlying LISA for armlengthaccurate
     double armlengthaccurate(int arm, double t) {
-	return basiclisa->armlengthaccurate(arm,t);
-    }
+	   return basiclisa->armlengthaccurate(arm,t);
+    };
 
     /// Calls underlying LISA for putn
     void putn(Vector &n, int arm, double t) {
-	basiclisa->putn(n,arm,t);
-    }
+	   basiclisa->putn(n,arm,t);
+    };
 
     /// Calls underlying LISA for putp        
     void putp(Vector &p, int craft, double t) {
-	basiclisa->putp(p,craft,t);
-    }
-
-    /// Calls underlying LISA for putn[]
-    void putn(double n[], int arm, double t) {
-	basiclisa->putn(n,arm,t);
-    }
-
-    /// Calls underlying LISA for putp[]
-    void putp(double p[], int craft, double t) {
-	basiclisa->putp(p,craft,t);
-    }
+	   basiclisa->putp(p,craft,t);
+    };
 
     // The following is specific to CacheLISA.
 
     /// Sets up counters for a new retardation.
     void newretardtime(double t) {
-	it = t; rt = t;
-	trb = 0.0; tra = 0.0;
-	rts = 0; hash = 0;
-	lastarm = 0;
-    }
+	   it = t; rt = t;
+	   trb = 0.0; tra = 0.0;
+	   rts = 0; hash = 0;
+	   lastarm = 0;
+    };
 
     /// Returns the retarded time so far.
     double retardedtime() {
-	return rt;
-    }
+	   return rt;
+    };
 
     double retardation() {
-	return trb + tra;
-    }
+	   return trb + tra;
+    };
 
     /// Computes a retardation. Contains the caching mechanism.
     void retard(int ret) {
-	if(ret == 0)
-	    return;
+        if(ret == 0) return;
 
-	// Update the retardation key and the hash.
-	if(rts == 0) {
-	    rts = (rts << 3) | (ret > 0 ? ret : (4 | -ret));
-	    hash = rts;
-	} else {
-	    rts = (rts << 3) | (ret > 0 ? ret : (4 | -ret));
+        // Update the retardation key and the hash.
+        if(rts == 0) {
+            rts = (rts << 3) | (ret > 0 ? ret : (4 | -ret));
+            hash = rts;
+        } else {
+            rts = (rts << 3) | (ret > 0 ? ret : (4 | -ret));
 	    
-	    hash = hash << 1;
-	    if(abs(ret) == abs(lastarm))
-		rts = rts | 1;
-	}
+            hash = hash << 1;
+            if(abs(ret) == abs(lastarm)) rts = rts | 1;
+        }
 	
-	lastarm = ret;
+        lastarm = ret;
 
-	// We cannot handle such as a large hash. Should throw exception.
-	if(hash >= buflength) {
-	    cout << "CacheLISA: Hash out of range in retard()!" << endl;
-	    abort();
-	}
+        // We cannot handle such as a large hash. Should throw exception.
+        if(hash >= buflength) {
+            std::cerr << "CacheLISA::retard: Hash " << hash << " out of range"
+                      << " [" << __FILE__ << ":" << __LINE__ << "]." << std::endl;
+	
+    		ExceptionOutOfBounds e;
+	       	throw e;
+        }
 
-	if(its[hash] == it && keys[hash] == rts) {
-	    // Got it!
+        if(its[hash] == it && keys[hash] == rts) {
+            // Got it!
 
-	    rt = rtis[hash];
-	    trb = trbs[hash]; tra = tras[hash];
-	} else {
-	    // Nah, will have to compute it.
+            rt = rtis[hash];
+            trb = trbs[hash]; tra = tras[hash];
+        } else {
+            // Nah, will have to compute it.
 
-	    trb += basiclisa->armlengthbaseline(ret,rt);
-	    tra += basiclisa->armlengthaccurate(ret,rt);
+            trb += basiclisa->armlengthbaseline(ret,rt);
+            tra += basiclisa->armlengthaccurate(ret,rt);
 		
-	    rt = (it - trb) - tra;
-	}
+            rt = (it - trb) - tra;
+        }
 
-	if(its[hash] < it) {
-	    // We can replace the old value in the cache with the
-	    // newly computed value. But we don't do this if the time
-	    // is the same, but the key is different (a cache collision).
+        if(its[hash] < it) {
+            // We can replace the old value in the cache with the
+            // newly computed value. But we don't do this if the time
+            // is the same, but the key is different (a cache collision).
 
-	    its[hash] = it;
-	    keys[hash] = rts;
+            its[hash] = it;
+            keys[hash] = rts;
 
-	    rtis[hash] = rt;
-	    trbs[hash] = trb; tras[hash] = tra;
-	}
-    }
+            rtis[hash] = rt;
+            trbs[hash] = trb; tras[hash] = tra;
+        }
+    };
 
     /** Computes a retardation with a different LISA geometry
 	[generally it will be basiclisa->physlisa()]. Note that
@@ -199,13 +201,13 @@ class CacheLISA : public LISA {
 	retard call */
 
     void retard(LISA *anotherlisa,int ret) {
-	if (ret != 0) {
-	    trb += anotherlisa->armlengthbaseline(ret,rt);  
-	    tra += anotherlisa->armlengthaccurate(ret,rt);
+        if (ret != 0) {
+            trb += anotherlisa->armlengthbaseline(ret,rt);  
+            tra += anotherlisa->armlengthaccurate(ret,rt);
 
-	    rt = (it - trb) - tra;
-	}
-    }
+            rt = (it - trb) - tra;
+        }
+    };
 };
 
 #endif /* _LISASIM_RETARD_H_ */
