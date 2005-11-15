1ae6e6d9ef9ce34e615bbf58cf92aa1a6ce58d4c
Michele Vallisneri <vallis@vallis.org>
2005-11-15 21:55:15 +0000

    Added except and signal header; removed noise
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@227 35f09e0b-191c-0410-822b-d9f812da8b64

--- lisasim/lisasim.h
+++ lisasim/lisasim.h
@@ -1,3 +1,9 @@
+/* $Id$
+ * $Date$
+ * $Author$
+ * $Revision$
+ */
+
 #ifndef _LISASIM_H_
 #define _LISASIM_H_
 
@@ -5,9 +11,10 @@
 #include "lisasim-tdi.h"
 #include "lisasim-tdinoise.h"
 #include "lisasim-tdisignal.h"
-#include "lisasim-noise.h"
 #include "lisasim-lisa.h"
 #include "lisasim-tens.h"
 #include "lisasim-retard.h"
+#include "lisasim-signal.h"
+#include "lisasim-except.h"
 
 #endif /* _LISASIM_H_ */
