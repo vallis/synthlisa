7d1d3ff1d6c715565323d73c7d2dd80fbdae624a
Michele Vallisneri <vallis@vallis.org>
2005-11-15 21:55:41 +0000

    First attempt at SynthLISA exception handling
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@228 35f09e0b-191c-0410-822b-d9f812da8b64

---
+++ lisasim/lisasim-except.h
@@ -0,0 +1,18 @@
+/* $Id$
+ * $Date$
+ * $Author$
+ * $Revision$
+ */
+
+#ifndef _LISASIM_EXCEPT_H_
+#define _LISASIM_EXCEPT_H_
+
+// later can be made to inherit from a more generic class
+
+class SynthLISAException {};
+
+class ExceptionOutOfBounds : SynthLISAException {};
+class ExceptionUndefined : SynthLISAException {};
+class ExceptionWrongArguments : SynthLISAException {};
+
+#endif /* _LISASIM_EXCEPT_H_ */
