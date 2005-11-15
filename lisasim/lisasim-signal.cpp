f353db4f3b8685becdad5f8af405674a7ca897d2
Michele Vallisneri <vallis@vallis.org>
2005-11-15 22:53:39 +0000

    First working version of new Signal/Noise architecture
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@234 35f09e0b-191c-0410-822b-d9f812da8b64

---
+++ lisasim/lisasim-signal.cpp
@@ -0,0 +1,508 @@
+/* $Id$
+ * $Date$
+ * $Author$
+ * $Revision$
+ */
+
+#include "lisasim-signal.h"
+#include "lisasim-except.h"
+
+#include <iostream>
+#include <math.h>
+
+// --- RingBuffer ---
+
+RingBuffer::RingBuffer(long len)
+	: data(new double[len]), length(len) {
+
+	reset();
+}
+
+RingBuffer::~RingBuffer() {
+	delete [] data;
+}
+
+void RingBuffer::reset() {
+	for(int i=0;i<length;i++)
+		data[i] = 0.0;
+}
+
+
+// --- BufferedSignalSource ---
+
+BufferedSignalSource::BufferedSignalSource(long len)
+	: buffer(len), length(len), current(-1) {}
+
+void BufferedSignalSource::reset(unsigned long seed) {
+	buffer.reset();
+	
+	current = -1;
+}
+
+double BufferedSignalSource::operator[](long pos) {
+	if (pos <= current - length) {
+		std::cerr << "BufferedSignalSource::operator[](long): stale sample access at "
+		          << pos << " [" << __FILE__ << ":" << __LINE__ << "]." << std::endl;
+
+		ExceptionOutOfBounds e;
+		throw e;
+	} else if (pos > current) {
+		for(int i=current+1;i<=pos;i++) {
+			buffer[i] = getvalue(i);
+		}
+		
+		current = pos;
+
+		return buffer[pos];
+	} else {
+		return buffer[pos];
+	}
+}
+
+
+// --- WhiteNoiseSource ---
+
+WhiteNoiseSource::WhiteNoiseSource(long len,unsigned long seed,double norm) 
+	: BufferedSignalSource(len), normalize(norm) {
+
+    randgen = gsl_rng_alloc(gsl_rng_ranlux);
+
+    seedrandgen(seed);
+}
+
+WhiteNoiseSource::~WhiteNoiseSource() {
+    gsl_rng_free(randgen);
+}
+
+void WhiteNoiseSource::seedrandgen(unsigned long seed) {
+    if (seed == 0) {
+		struct timeval tv;
+	
+		gettimeofday(&tv,0);
+	  
+		gsl_rng_set(randgen,tv.tv_sec+tv.tv_usec);
+    } else {
+		gsl_rng_set(randgen,seed);
+    }
+
+    cacheset = 0;
+    cacherand = 0;
+}
+
+void WhiteNoiseSource::reset(unsigned long seed) {
+	seedrandgen(seed);
+	
+	BufferedSignalSource::reset(seed);
+}
+
+/* Box-Muller transform to get Gaussian deviate from uniform random,
+   number, adapted from GSL 1.4 randist/gauss.c
+
+   Since pos is not actually used, this will give sensible results only
+   within a buffered environment */
+
+double WhiteNoiseSource::getvalue(long pos) {
+  double x, y, r2;
+
+  if (cacheset == 0) {
+      do {
+		  x = -1.0 + 2.0 * gsl_rng_uniform(randgen);
+		  y = -1.0 + 2.0 * gsl_rng_uniform(randgen);
+		  
+		  r2 = x * x + y * y;
+      } while (r2 > 1.0 || r2 == 0);
+
+      double root = sqrt (-2.0 * log (r2) / r2);
+
+      cacheset = 1;
+      cacherand = x * root;
+
+      return normalize * y * root;
+  } else {
+      cacheset = 0;
+      return normalize * cacherand;
+  }
+}
+
+
+// --- SampledSignalSource ---
+
+// does not own the data array!
+
+SampledSignalSource::SampledSignalSource(double *darray,long len,double norm)
+	: data(darray), length(len), normalize(norm) {}
+
+// will pad with zeros on the negative index side
+
+double SampledSignalSource::operator[](long pos) {
+	if(pos < 0) {
+		return 0.0;
+	} else if(pos >= length) {
+		std::cerr << "SampledSignalSource::operator[](long): index too large at "
+		          << pos << " [" << __FILE__ << ":" << __LINE__ << "]." << std::endl;
+
+		ExceptionOutOfBounds e;
+		throw e;
+	} else {
+		return normalize * data[pos];
+	}
+}
+
+
+// --- Filters ---
+
+double NoFilter::getvalue(SignalSource &x,SignalSource &y,long pos) {
+	return x[pos];
+}
+
+
+IntFilter::IntFilter(double a)
+	: alpha(a) {}
+
+double IntFilter::getvalue(SignalSource &x,SignalSource &y,long pos) {
+	return alpha * y[pos - 1] + x[pos];
+}
+
+
+double DiffFilter::getvalue(SignalSource &x,SignalSource &y,long pos) {
+	return x[pos] - x[pos-1];
+}
+
+
+/* Note that FIR makes a copy of the coefficient array. Normally filters
+   are defined so that a[0] = 1 */
+
+FIRFilter::FIRFilter(double *aarray,int len)
+	: a(new double[len]), length(len) {
+		
+	for(int i=0;i<length;i++)
+		a[i] = aarray[i];
+}
+
+FIRFilter::~FIRFilter() {
+ 	delete [] a;	
+}
+
+double FIRFilter::getvalue(SignalSource &x,SignalSource &y,long pos) {
+	double acc = 0.0;
+	
+	for(int i=0;i<length;i++)
+		acc += a[i] * x[pos-i];
+
+	return acc;
+}
+
+
+/* Note that IIR makes copies of the coefficient arrays. Normally filters
+   are defined so that a[0] = 1 and b[0] = 0; b[0] is not used, anyway */
+
+IIRFilter::IIRFilter(double *aarray,int lena,double *barray,int lenb)
+	: a(new double[lena]), b(new double[lenb]), lengtha(lena), lengthb(lenb) {
+
+	for(int i=0;i<lena;i++)
+		a[i] = aarray[i];
+
+	for(int j=0;j<lenb;j++)
+		b[j] = barray[j];		
+}
+
+IIRFilter::~IIRFilter() {
+	delete [] b;
+	delete [] a;
+}
+
+double IIRFilter::getvalue(SignalSource &x,SignalSource &y,long pos) {
+	double acc = 0.0;
+	
+	for(int i=0;i<lengtha;i++)
+		acc += a[i] * x[pos-i];
+
+	for(int j=1;j<lengthb;j++)
+		acc += b[j] * y[pos-j];
+
+	return acc;
+}
+
+
+// --- SignalFilter ---
+
+SignalFilter::SignalFilter(long len,SignalSource *src,Filter *flt,double norm)
+	: BufferedSignalSource(len), source(src), filter(flt), normalize(norm) {}
+
+void SignalFilter::reset(unsigned long seed) {
+	source->reset(seed);
+	
+	BufferedSignalSource::reset(seed);
+}
+
+double SignalFilter::getvalue(long pos) {
+	return filter->getvalue(*source,*this,pos);
+}
+
+double SignalFilter::operator[](long pos) {
+	return normalize * BufferedSignalSource::operator[](pos);
+}
+
+
+// --- Interpolators ---
+
+double NearestInterpolator::getvalue(SignalSource &y,long ind,double dind) {
+	return (dind < 0.5 ? y[ind] : y[ind+1]);
+}
+
+
+// 0 < dind < 1; the desired sample would be at ind + dind
+
+double LinearInterpolator::getvalue(SignalSource &y,long ind,double dind) {
+	return (1.0 - dind) * y[ind] + dind * y[ind+1];
+}
+
+
+// this is to use only "old" values, with (implicitly) 1 < dind < 2
+
+double LinearExtrapolator::getvalue(SignalSource &y,long ind,double dind) {
+	return (-dind) * y[ind-1] + (1.0 + dind) * y[ind];
+}
+
+
+double LagrangeInterpolator::getvalue(SignalSource &y,long ind,double dind) {
+	for(int i=0;i<semiwindow;i++) {
+    	ya[semiwindow-i] = y[ind-i];
+    	ya[semiwindow+i+1] = y[ind+i+1];
+	}
+
+	return polint(semiwindow+dind);
+}
+
+LagrangeInterpolator::LagrangeInterpolator(int semiwin)
+    : xa(new double[2*semiwin+1]), ya(new double[2*semiwin+1]),
+       c(new double[2*semiwin+1]),  d(new double[2*semiwin+1]) {
+		
+	for(int i=1;i<=window;i++) {
+		xa[i] = 1.0*i;
+		ya[i] = 0.0;
+	}    
+}
+
+LagrangeInterpolator::~LagrangeInterpolator() {
+    delete [] d;
+    delete [] c;
+	
+    delete [] ya;
+    delete [] xa;
+}
+
+double LagrangeInterpolator::polint(double x) {
+    int n = window;
+    int i,m,ns=1;
+    double den,dif,dift,ho,hp,w;
+
+    double res,dres; // dres is the error estimate
+
+    dif=fabs(x-xa[1]);
+
+    for (i=1;i<=n;i++) {
+		if ( (dift=fabs(x-xa[i])) < dif) {
+			ns=i;
+			dif=dift;
+		}
+	
+		c[i]=ya[i];
+		d[i]=ya[i];
+    }
+
+    res=ya[ns--];
+
+    for (m=1;m<n;m++) {
+		for (i=1;i<=n-m;i++) {
+			ho=xa[i]-x;
+			hp=xa[i+m]-x;
+			w=c[i+1]-d[i];
+			den=ho-hp;
+			den=w/den;
+			d[i]=hp*den;
+			c[i]=ho*den;
+		}
+	
+		res += (dres=(2*ns < (n-m) ? c[ns+1] : d[ns--]));
+    }
+
+    return res;
+}
+
+
+// getInterpolator
+
+Interpolator *getInterpolator(int interplen) {
+	if (interplen == 0)
+    	return new NearestInterpolator();
+	else if (interplen == -1)
+		return new LinearExtrapolator();
+	else if (interplen == 1)
+		return new LinearInterpolator();
+	else if (interplen > 0)
+		return new LagrangeInterpolator(interplen);
+	else {
+		std::cerr << "getInterpolator(...): undefined interpolator length "
+		          << interplen << " [" << __FILE__ << ":" << __LINE__ << "]." << std::endl;
+	
+		ExceptionUndefined e;
+		throw e;
+	}
+}
+
+
+// InterpolatedSignal
+
+InterpolatedSignal::InterpolatedSignal(SignalSource *src,Interpolator *inte,
+									   double deltat,double prebuffer,double norm)
+	: source(src), interp(inte),
+	  samplingtime(deltat), prebuffertime(prebuffer), normalize(norm) {}
+
+void InterpolatedSignal::reset(unsigned long seed) {
+	source->reset(seed);
+}
+
+double InterpolatedSignal::value(double time) {
+	if (normalize == 0.0) return 0.0;
+
+	try {
+		double ireal, iint, ifrac;
+
+		ireal = (time + prebuffertime) / samplingtime;
+		iint  = floor(ireal);
+		ifrac = ireal - iint;
+
+		return normalize * interp->getvalue(*source,long(iint),ifrac);
+	} catch (ExceptionOutOfBounds &e) {
+		std::cerr << "InterpolateSignal::value(double) : OutOfBounds while accessing "
+		          << time << " [" << __FILE__ << ":" << __LINE__ << "]." << std::endl;
+		
+		throw e;
+	}
+}
+
+// do everything much more carefully (and slowly) to avoid rounding error
+// there may be a more efficient way to do this
+
+double InterpolatedSignal::value(double timebase,double timecorr) {
+	try {
+		double irealb, iintb, ifracb;
+		double irealc, iintc, ifracc;
+		double ifrac;
+
+		irealb = (timebase + prebuffertime) / samplingtime;
+		iintb  = floor(irealb);
+		ifracb = irealb - iintb;
+
+		irealc = timecorr / samplingtime;
+		iintc  = floor(irealc);
+		ifracc = irealc - iintc;
+
+		ifrac = ifracb + ifracc;
+
+		if (ifrac >= 1.0) {
+			return normalize * interp->getvalue(*source,long(iintb+iintc)+1,ifrac-1.0);
+		} else {
+			return normalize * interp->getvalue(*source,long(iintb+iintc),ifrac);
+		}
+	} catch (ExceptionOutOfBounds &e) {
+		std::cerr << "InterpolateSignal::value(double,double): OutOfBounds while accessing "
+		          << "(" << timebase << "," << timecorr << ")"
+				  << " [" << __FILE__ << ":" << __LINE__ << "]." << std::endl;
+		
+		throw e;
+	}
+}
+
+void InterpolatedSignal::setinterp(Interpolator *inte) {
+	interp = inte;
+}
+
+
+// PowerLawNoise
+
+PowerLawNoise::PowerLawNoise(double deltat,double prebuffer,
+				double psd,double exponent,int interplen,unsigned long seed) {
+
+	double nyquistf = 0.5 / deltat;
+	double normalize;
+	
+	if (exponent == 0.00) {
+		filter = new NoFilter();
+		normalize = sqrt(psd) * sqrt(nyquistf);
+	} else if (exponent == 2.00) {
+		filter = new DiffFilter();
+		normalize = sqrt(psd) * sqrt(nyquistf) / (2.00 * M_PI * deltat);
+	} else if (exponent == -2.00) {
+		filter = new IntFilter();
+        normalize = sqrt(psd) * sqrt(nyquistf) * (2.00 * M_PI * deltat);
+	} else {
+		std::cerr << "PowerLawNoise::PowerLawNoise(...): undefined PowerLaw exponent "
+		          << exponent << " [" << __FILE__ << ":" << __LINE__ << "]." << std::endl;
+
+		ExceptionUndefined e;
+		throw e;
+	}
+
+	whitenoise = new WhiteNoiseSource(long(prebuffer/deltat+32),seed);
+	filterednoise = new SignalFilter(long(prebuffer/deltat+32),whitenoise,filter,normalize);
+
+	try {
+		interp = getInterpolator(interplen);	
+	} catch (ExceptionUndefined &e) {
+		std::cerr << "PowerLawNoise::PowerLawNoise(...): undefined interpolator length "
+		          << interplen << " [" << __FILE__ << ":" << __LINE__ << "]." << std::endl;
+
+		// ??? if this exception is thrown, will the destructor be called?
+		throw e;		
+	}
+
+	interpolatednoise = new InterpolatedSignal(filterednoise,interp,deltat,prebuffer);
+}
+
+PowerLawNoise::~PowerLawNoise() {
+	delete interpolatednoise;
+	delete interp;
+	delete filterednoise;
+	delete whitenoise;
+	delete filter;
+}
+
+void PowerLawNoise::reset(unsigned long seed) {
+	interpolatednoise->reset(seed);
+}
+
+
+// SampledSignal
+
+SampledSignal::SampledSignal(double *narray,long length,double deltat,double prebuffer,
+	double norm,Filter *filter,int interplen) {
+
+	try {
+		interp = getInterpolator(interplen);	
+	} catch (ExceptionUndefined &e) {
+		std::cerr << "SampledSignal::SampledSignal(...): undefined interpolator length "
+		          << interplen << " [" << __FILE__ << ":" << __LINE__ << "]." << std::endl;
+
+		throw e;
+	}
+
+	samplednoise = new SampledSignalSource(narray,length,norm);
+
+	if (!filter) {
+		filteredsamples = 0;
+		interpolatednoise = new InterpolatedSignal(samplednoise,interp,deltat,prebuffer);		
+	} else {
+		filteredsamples = new SignalFilter(long(prebuffer/deltat+32),samplednoise,filter);
+		interpolatednoise = new InterpolatedSignal(filteredsamples,interp,deltat,prebuffer);
+	}
+}
+
+SampledSignal::~SampledSignal() {
+	delete interpolatednoise;
+	delete filteredsamples;
+	delete samplednoise;
+	delete interp;
+}
+
---
+++ lisasim/lisasim-signal.h
@@ -0,0 +1,353 @@
+/* $Id$
+ * $Date$
+ * $Author$
+ * $Revision$
+ */
+ 
+#ifndef _LISASIM_SIGNAL_H_
+#define _LISASIM_SIGNAL_H_
+
+
+#include <stdlib.h>
+
+class RingBuffer {
+ private:
+    double *data;
+    long length;
+
+ public:
+	RingBuffer(long len);
+	~RingBuffer();
+	
+	void reset();
+	
+	inline double& operator[](long pos);
+};
+
+inline double& RingBuffer::operator[](long pos) {
+	if(pos < 0) pos += length * (abs(pos)/length + 1);
+
+	return data[pos % length];
+}
+
+
+/* SignalSource interface: can return [pos], may be reset to beginning,
+   will throw exception (ExceptionOutOfBounds) at EOF or for stale 
+   access */
+
+class SignalSource {
+ public:
+	virtual ~SignalSource() {};
+
+	virtual void reset(unsigned long seed = 0) {};
+	virtual double operator[](long pos) = 0;
+};
+
+
+/* BufferedSignalSource interface: inherit from it, redefine
+   getvalue(pos), which will be called only once to get each new sample */
+
+class BufferedSignalSource : public SignalSource {
+ private:
+	RingBuffer buffer;
+	long length;
+
+	long current;
+
+ public:
+	BufferedSignalSource(long len);
+	virtual ~BufferedSignalSource() {}; // ??? would "= 0" do here?
+
+	virtual double getvalue(long pos) = 0;
+
+	virtual void reset(unsigned long seed = 0); // ??? redefining default
+	virtual double operator[](long pos);
+};
+
+
+/* WhiteNoiseSource uses the GSL random number routines; perhaps there
+   is something more efficient. */
+
+#include "GSL/gsl_rng.h"
+
+class WhiteNoiseSource : public BufferedSignalSource {
+ private:	
+    gsl_rng *randgen;
+
+    int cacheset;
+    double cacherand;
+
+	double normalize;
+
+    void seedrandgen(unsigned long seed);
+    double deviate();
+
+ public:
+	WhiteNoiseSource(long len,unsigned long seed = 0,double norm = 1.0);
+	~WhiteNoiseSource();	
+
+	double getvalue(long pos);
+		
+	void reset(unsigned long seed = 0);  // ??? redefining default
+};
+
+
+class SampledSignalSource : public SignalSource {
+ private:
+	double *data;
+	long length;
+
+	double normalize;
+
+ public:
+	SampledSignalSource(double *darray,long len,double norm = 1.0);
+
+	double operator[](long pos);
+};
+
+
+/* Filters! It might be tempting to inline the getvalue() call, but it
+   would probably do little good since they are called as virtual methods
+   from a base pointer. */
+
+class Filter {
+ public:
+    virtual ~Filter() {};
+ 
+    virtual double getvalue(SignalSource &x,SignalSource &y,long pos) = 0;
+};
+
+
+class NoFilter : public Filter {
+ public:
+    double getvalue(SignalSource &x,SignalSource &y,long pos);
+};
+
+
+class IntFilter : public Filter {
+ private:
+    double alpha;
+
+ public:
+    IntFilter(double a = 0.9999);
+
+    double getvalue(SignalSource &x,SignalSource &y,long pos);
+};
+
+
+class DiffFilter : public Filter {
+ public:
+    double getvalue(SignalSource &x,SignalSource &y,long pos);
+};
+
+
+class FIRFilter : public Filter {
+ private:
+ 	double *a;
+ 	int length;
+
+ public:
+ 	FIRFilter(double *aarray,int length);
+ 	~FIRFilter();
+
+    double getvalue(SignalSource &x,SignalSource &y,long pos);
+};
+
+
+class IIRFilter : public Filter {
+ private:
+ 	double *a, *b;
+ 	int lengtha, lengthb;
+
+ public:
+ 	IIRFilter(double *aarray,int lena,double *barray,int lenb);
+ 	~IIRFilter();
+ 
+    double getvalue(SignalSource &x,SignalSource &y,long pos);
+};
+
+
+class SignalFilter : public BufferedSignalSource {
+ private:
+	SignalSource *source;
+	Filter *filter;
+
+	double normalize;
+
+ public:
+	SignalFilter(long length,SignalSource *src,Filter *flt,double norm = 1.0);
+
+	double getvalue(long pos);
+	
+	void reset(unsigned long seed = 0);  // ??? redefining default
+	double operator[](long pos);
+};
+
+
+/* Interface for Signal: value(time) and value(timebase,timecorr). Also
+   reset(). */
+
+class Signal {
+ public:
+	virtual ~Signal() {};
+
+    virtual void reset(unsigned long seed = 0) {};  // ??? redefining default
+
+	virtual double value(double time) = 0; 
+
+	// standard implementation if we are not more precise than this
+	virtual double value(double timebase,double timecorr) {
+		return value(timebase + timecorr);
+	};
+
+	// for backward compatibility
+
+	virtual double operator[](double time) { return value(time); };
+	virtual double operator()(double timebase,double timecorr) { return value(timebase,timecorr); }
+	
+    virtual double noise(double time) { return value(time); };    
+	virtual double noise(double timebase,double timecorr) { return value(timebase,timecorr); }
+};
+
+
+// With this typedef, Signal = Noise for backward compatibility
+
+typedef Signal Noise;
+
+
+// Interpolators!
+
+class Interpolator {
+ public:
+    virtual double getvalue(SignalSource &y,long ind,double dind) = 0;
+};
+
+
+class NearestInterpolator : public Interpolator {
+ public:
+	double getvalue(SignalSource &y,long ind,double dind);
+};
+
+
+class LinearInterpolator : public Interpolator {
+ public:
+	double getvalue(SignalSource &y,long ind,double dind);
+};
+
+
+class LinearExtrapolator : public Interpolator {
+ public:
+	double getvalue(SignalSource &y,long ind,double dind);
+};
+
+
+class LagrangeInterpolator : public Interpolator {
+ private:
+    int window, semiwindow;
+
+    double *xa,*ya;
+    double *c,*d;
+
+    double polint(double x);
+
+ public:
+    LagrangeInterpolator(int semiwin);
+    virtual ~LagrangeInterpolator();
+
+	double getvalue(SignalSource &y,long ind,double dind);
+};
+
+
+// get one of the above by choosing its length (-1 for Extrapolator)
+
+Interpolator *getInterpolator(int interplen);
+
+
+// InterpolatedSignal
+
+class InterpolatedSignal : public Signal {
+ private:
+	SignalSource *source;
+	Interpolator *interp;
+	
+	double samplingtime, prebuffertime,normalize;
+	
+ public:
+	InterpolatedSignal(SignalSource *src,Interpolator *inte,
+					   double deltat,double prebuffer = 0.0,double norm = 1.0);
+
+    void reset(unsigned long seed = 0);  // ??? redefining default
+
+	double value(double time);
+	double value(double timebase,double timecorr);
+	
+	void setinterp(Interpolator *inte);
+};
+
+
+// PowerLawNoise
+
+class PowerLawNoise : public Signal {
+ private:
+	WhiteNoiseSource *whitenoise;
+	
+	Filter *filter;
+	SignalFilter *filterednoise;
+
+	Interpolator *interp;
+	InterpolatedSignal *interpolatednoise;
+
+ public:
+	PowerLawNoise(double deltat,double prebuffer,
+		double psd,double exponent,int interplen = 1,unsigned long seed = 0);
+	~PowerLawNoise();
+
+	void reset(unsigned long seed = 0);  // ??? redefining default
+
+	double value(double time);
+	double value(double timebase,double timecorr);
+};
+
+inline double PowerLawNoise::value(double time) {
+	return interpolatednoise->value(time);
+}
+
+inline double PowerLawNoise::value(double timebase,double timecorr) {
+	return interpolatednoise->value(timebase,timecorr);
+}
+
+
+// SampledSignal
+
+class SampledSignal : public Signal {
+ private:
+	SampledSignalSource *samplednoise;
+
+	SignalFilter *filteredsamples;
+
+	Interpolator *interp;
+	InterpolatedSignal *interpolatednoise;
+
+ public:
+	SampledSignal(double *narray,long length,double deltat,double prebuffer,
+		double norm = 1.0,Filter *filter = 0,int interplen = 1);
+	~SampledSignal();
+
+    // nothing to reset...
+
+	double value(double time);
+	double value(double timebase,double timecorr);
+};
+
+inline double SampledSignal::value(double time) {
+	return interpolatednoise->value(time);
+}
+
+inline double SampledSignal::value(double timebase,double timecorr) {
+	return interpolatednoise->value(timebase,timecorr);
+}
+
+
+
+
+#endif /* _LISASIM_SIGNAL_H_ */
