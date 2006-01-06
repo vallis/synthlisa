9855d830b60da3bdf69c3c1324c58bb1331b83e9
Michele Vallisneri <vallis@vallis.org>
2006-01-06 19:22:06 +0000

    Corrected for new Numeric and SWIG versions
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@266 35f09e0b-191c-0410-822b-d9f812da8b64

--- cygwin-install.sh
+++ cygwin-install.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+# $Id$
+
 NUMERIC=`ls Numeric-*.tar.gz | tail -1`
 
-tar zxvf $NUMERIC
+if [ -z $NUMERIC ]
+then
+    NUMERIC=`ls Numeric-*.tar | tail -1`
+
+    if [ -z $NUMERIC ]
+    then
+        echo "Can't find Numeric tar in this directory!"
+        return 1
+    else
+        tar xvf $NUMERIC
+    fi
+else
+    tar zxvf $NUMERIC
+fi
 
 NUMERICDIR=`ls | grep Numeric | head -1`
 THISDIR=`pwd`
--- default-install.sh
+++ default-install.sh
@@ -1,13 +1,50 @@
 #!/bin/sh
 
+# $Id$
+
 NUMERIC=`ls Numeric-*.tar.gz | tail -1`
+
+if [ -z $NUMERIC ]
+then
+    NUMERIC=`ls Numeric-*.tar | tail -1`
+
+    if [ -z $NUMERIC ]
+    then
+        echo "Can't find Numeric tar in this directory!"
+        exit 1
+    else
+        tar xvf $NUMERIC
+    fi
+else
+    tar zxvf $NUMERIC
+fi
+
 SWIG=`ls swig-*.tar.gz | tail -1`
 
-tar zxvf $NUMERIC
-tar zxvf $SWIG
+if [ -z $SWIG ]
+then
+    SWIG=`ls swig-*.tar | tail -1`
 
-NUMERICDIR=`ls | grep Numeric | head -1`
-SWIGDIR=`ls | grep SWIG | head -1`
+    if [ -z $SWIG ]
+    then
+        echo "Can't find SWIG in this directory!"
+        exit 1
+    else
+        tar xvf $SWIG
+    fi
+else
+    tar zxvf $SWIG
+fi
+
+NUMERICDIR=`ls | grep Numeric | grep -v tar | head -1`
+
+SWIGDIR=`ls | grep swig | grep -v tar | head -1`
+
+if [ -z $SWIGDIR ]
+then
+    SWIGDIR=`ls | grep SWIG | head -1`
+fi
+
 THISDIR=`pwd`
 
 cd $NUMERICDIR
