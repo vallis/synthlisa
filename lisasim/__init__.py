b4a5ee6ccbd1e2ea8fbf49e383318ce6688a6847
Michele Vallisneri <vallis@vallis.org>
2006-02-14 22:47:14 +0000

    import mpi
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@285 35f09e0b-191c-0410-822b-d9f812da8b64

--- lisasim/__init__.py
+++ lisasim/__init__.py
@@ -6,3 +6,9 @@
 from lisaswig import *
 from lisautils import *
 from lisaxml import *
+
+try:
+    from lisapar import *
+except ImportError:
+    # swallow the ImportError if mpi is not installed
+    pass
