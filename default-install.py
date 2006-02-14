669fb814254023443a8328c991ea638406d6b7e0
Michele Vallisneri <vallis@vallis.org>
2006-02-14 22:45:18 +0000

    More install options, MMPI
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@282 35f09e0b-191c-0410-822b-d9f812da8b64

--- default-install.py
+++ default-install.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
+import sys
 import os
 import os.path
 import glob
@@ -29,18 +30,47 @@ def findpackage(packagename,dirname):
     print "Using unpacking dir " + dir[-1]
     return dir[-1]
 
+# if we're given a command-line argument, try to parse it to decide what to
+# install; otherwise install everything that we find
+
+if len(sys.argv) > 1:
+	donumeric,doswig,dorxp,dopyx,dosynthlisa,dommpi = 0,0,0,0,0,0
+
+	for arg in sys.argv:
+		if arg == 'Numeric' or arg == 'numeric':
+			donumeric = 1
+		elif arg == 'swig' or arg == 'SWIG':
+			doswig = 1
+		elif arg == 'rxp' or arg == 'RXP' or arg == 'pyRXP':
+			dorxp = 1
+		elif arg == 'pyx' or arg == 'PyX':
+			dopyx = 1
+		elif arg == 'synthlisa' or arg == 'synthLISA':
+			dosynthlisa = 1
+		elif arg == 'mmpi' or arg == 'mpi' or arg == 'MMPI' or arg == 'MPI':
+			dommpi = 1
+else:
+	# don't do MMPI by default
+	donumeric,doswig,dorxp,dopyx,dosynthlisa,dommpi = 1,1,1,1,1,0
+
 thisdir = os.getcwd()
 
 os.chdir('packages')
 
 pckgdir = os.getcwd()
 
-numericdir = findpackage('Numeric','Numeric')
-swigdir    = findpackage('swig','SWIG')
-rxpdir     = findpackage('pyRXP','pyRXP')
-pyxdir     = findpackage('PyX','PyX')
+if donumeric:
+	numericdir = findpackage('Numeric','Numeric')
+if doswig:
+	swigdir    = findpackage('swig','SWIG')
+if dorxp:
+	rxpdir     = findpackage('pyRXP','pyRXP')
+if dopyx:
+	pyxdir     = findpackage('PyX','PyX')
+if dommpi:
+	mmpidir    = findpackage('mikempi','mikempi')
 
-if numericdir:
+if donumeric and numericdir:
     try:
         os.chdir(numericdir)    
         assert os.system('python setup.py install --prefix=' + thisdir) == 0
@@ -49,7 +79,7 @@ if numericdir:
         print "Error while compiling and installing Numeric"
         raise
 
-if swigdir:
+if doswig and swigdir:
     try:
         os.chdir(swigdir)
         assert os.system('./configure --prefix=' + thisdir) == 0
@@ -60,7 +90,7 @@ if swigdir:
         print "Error while compiling and installing SWIG"
         raise
 
-if pyxdir:
+if dopyx and pyxdir:
     try:
         os.chdir(pyxdir)
         assert os.system('python setup.py install --prefix=. --root=' + thisdir) == 0
@@ -69,7 +99,7 @@ if pyxdir:
         print "Error while compiling and installing PyX"
         raise
         
-if rxpdir:
+if dorxp and rxpdir:
     try:
         os.chdir(rxpdir + '/pyRXP')
         assert os.system('python setup.py install --prefix=' + thisdir) == 0
@@ -78,11 +108,24 @@ if rxpdir:
         print "Error while compiling and installing pyRXP"
         raise
 
+if dommpi and mmpidir:
+	try:
+		assert sys.version_info[0] + 0.1*sys.version_info[1] >= 2.4, "Need at least Python 2.4 to compile MMPI!"
+		assert os.system('mpicc --version > /dev/null') == 0, "Need to have mpicc in your path!"
+		
+		os.chdir(mmpidir)
+		assert os.system('python setup.py install --prefix=' + thisdir + ' --with-numeric=' + thisdir) == 0 
+		os.chdir(pckgdir)
+	except AssertionError:
+		print "Error while compiling and installing MMPI"
+		raise
+
 os.chdir(thisdir)
         
 try:
-    assert os.system(('python setup.py install --with-numeric=' + thisdir +
-                      ' --with-swig=' + thisdir + '/bin/swig --prefix=' + thisdir)) == 0
+    if dosynthlisa:
+	    assert os.system(('python setup.py install --with-numeric=' + thisdir +
+    	                  ' --with-swig=' + thisdir + '/bin/swig --prefix=' + thisdir)) == 0
 except AssertionError:
     print "Error while compiling and installing synthLISA"
     raise
