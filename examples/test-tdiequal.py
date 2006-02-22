15621c55bcf6b7bb008ab6edd5360efa6e88caf4
Michele Vallisneri <vallis@vallis.org>
2006-02-22 22:10:54 +0000

    Added time series for time, as needed for plotting
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@298 35f09e0b-191c-0410-822b-d9f812da8b64

--- examples/test-tdiequal.py
+++ examples/test-tdiequal.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # $Id$
 # $Date$
@@ -12,9 +12,10 @@
 # - creating a LISA geometry object
 # - creating a TDInoise object based on standard pseudorandom noises
 # - defining a short hand for a basic Doppler observable y_slr,d1...dn expression
-# - calling getobs to get a 2D array of several TDI observables at equispaced times
+# - calling getobsc to get a 2D array of several TDI observables at equispaced times
+# - writing the time series to an XML file
 # - getting the spectra of the time series
-# - writing the spectra to disk
+# - writing the spectra to an XML file
 
 # import all the libraries that are needed
 
@@ -49,35 +50,43 @@ samples = 2**16    # 2**18 takes 60 s on a 1.25GHz G4; 2**21 used for plot
 
 patches = 256
 
-# getting the value of one of the basic Doppler observables requires a little trick:
-# here send = 2, link = 3, recv = 1, and all the seven possible delays are set to 0
-# with a similar syntax (but eight delays) you could also get the z's
+# getting the value of one of the basic Doppler observables requires a
+# little trick: here send = 2, link = 3, recv = 1, and all the seven
+# possible delays are set to 0 with a similar syntax (but eight delays)
+# you could also get the z's
 
 originalTDI.y231 = lambda t : originalTDI.y(2,3,1,0,0,0,0,0,0,0,t)
 
 # notice the call to getobs with a list "[...]" of observables
-# the result is a 2D array with four columns, corresponding to the four observables
+# (originalTDI.time, or originalTDI.t is just the time); the result is
+# a 2D array with six columns, corresponding to time and the five
+# observables
+
+# getobsc (as opposed to getobs) prints status information during evaluation
  
-observables = getobsc(samples,stime,[originalTDI.X,
+observables = getobsc(samples,stime,[originalTDI.time,
+                                     originalTDI.X,
                                      originalTDI.alpha,
                                      originalTDI.zeta,
                                      originalTDI.U,
                                      originalTDI.y231])
 
+# create the output XML file
+
 outputXML = lisaXML('data/tdiequal',
                     author='Michele Vallisneri',
                     comments='OriginalLISA (stationary equal-arm) TDI noises')
 
-# save time series to disk
+# save time series to the XML file
 
-outputXML.TDIData(observables,samples,stime,'Xf,alphaf,zetaf,Uf,y231f')
+outputXML.TDIData(observables,samples,stime,'t,Xf,alphaf,zetaf,Uf,y231f')
 
 # collect the columns of 'observables' in single-column arrays
 # for each observable
 
-[noiseX,noisea,noisez,noiseu,noisey] = transpose(observables)
+[time,noiseX,noisea,noisez,noiseu,noisey] = transpose(observables)
 
-# compute the spectra and write them to disk
+# compute the spectra and save them to the XML file
 
 myspecX = spect(noiseX,stime,patches)
 myspeca = spect(noisea,stime,patches)
@@ -88,11 +97,23 @@ myspecy = spect(noisey,stime,patches)
 allspec = transpose([myspecX[:,0],myspecX[:,1],
                                   myspeca[:,1],
                                   myspecz[:,1],
-                                  myspecu[:,1]])
+                                  myspecu[:,1],
+                                  myspecy[:,1]])
 
-outputXML.TDISpectraSelfDescribed(allspec[1:],'f,Xf,alphaf,zetaf,uf',encoding='Text')
+# the first column of the spectra contains the frequency abscissa, so
+# information about minimum and maximum frequency and about frequency
+# cadence may be gotten there; we omit the DC component, which may be
+# problematic to plot log-log
 
-# all data is written only on closing the outputXML; thus the arrays referenced
-# in the TDIData and TDISpectra calls should not be altered in the meantime
+outputXML.TDISpectraSelfDescribed(allspec[1:],'f,Xf,alphaf,zetaf,Uf,y231f',encoding='Text')
+
+# all data is written only on closing the outputXML; thus the arrays
+# referenced in the TDIData and TDISpectra calls should not be altered
+# in the meantime
 
 outputXML.close()
+
+print "You can plot the results of this script by running"
+print "  ./plotxml.py data/tdiequal.xml eps/tdiequal-spectra.eps"
+print "or"
+print "  ./plotxml.py data/tdiequal.xml eps/tdiequal-spectra.pdf"
