e1e78130c000b7118ace2f796fb72e93ed103af7
Michele Vallisneri <vallis@vallis.org>
2006-04-03 22:20:35 +0000

    Writes to XML, computes SN with lisautils.py function
    git-svn-id: svn+ssh://newman.caltech.edu/newman/user1/vallis/svnarchive/synthlisa/trunk@309 35f09e0b-191c-0410-822b-d9f812da8b64

--- examples/test-binary.py
+++ examples/test-binary.py
@@ -1,178 +1,121 @@
-#!/usr/bin/python
-
-# test of first-generation TDI signals for a monochromatic-binary GW source
-
-# this script demonstrates the generation of a TDI time series for a
-# GW signal, and in particular:
-#
-# - creating a LISA geometry object
-# - creating a Wave GW object
-# - creating a TDIsignal object based on the Wave object
-# - creating a standard TDInoise object
-# - calling getobs to get an array of the TDI X observable at equispaced times
-# - calling getobs to get an array of the TDI X noise at equispaced times
-# - computing the optimal S/N for the source
-
-# import all the libraries that are needed
+#!/usr/bin/env python
 
 from synthlisa import *
 from Numeric import *
 
-
-###################################################
-# creating the GW source from physical parameters #
-###################################################
-
-# the following parameters are taken from the standard test source
-# for the LISA simulator, v. 2.0 (Newtonian.c)
-
 import math
 
-# masses of the binary components (kg)
+# this function returns a PhysicalBinary object of given frequency, 
 
-Msun = 1.9889e30
+def PhysicalBinary(freq,phi0,inc,amp,elat,elon,psi):
+    """Returns a SimpleBinary Wave object of frequency freq [Hz],
+    initial phase phi0 [0,2pi], inclination inc [0,2pi], ecliptic latitude
+    and longitude elat [-pi/2,pi/2] and elon [0,2pi], and polarization
+    angle psi [0,pi]. The amplitude is compute from the two masses [given
+    in Msun] and the distance [given in Kpc]; these three parameters are
+    passed as the tuple amp = (m1,m2,dist)."""
 
-M1 = 0.5*Msun
-M2 = 0.033*Msun
+    G = 6.67259e-11
+    c = 2.99792458e8
 
-# luminosity distance (m)
+    kpc = 3.0856675807e19
+    Msun = 1.9889e30
 
-kpc = 3.0856675807e19
-r = 0.1*kpc
+    M1 = amp[0]*Msun
+    M2 = amp[1]*Msun
 
-# orbital frequency (Hz)
+    r = amp[2]*kpc
 
-forb = 1.0/1028.76
-montanafgw = 2.0*forb
+    R = (G*(M1+M2)*(1.0/(2.0*math.pi*freq/2))**2)**(1.0/3.0)
+    ampl = (2.0*M1*M2/(r*R) * (G/(c*c))**2)
 
-# find overall amplitude (G is mks, c is m/s)
+    return SimpleBinary(freq, phi0, inc, ampl, elat, elon, psi)
 
-G = 6.67259e-11
-c = 2.99792458e8
 
-R = (G*(M1+M2)*(1.0/(2.0*math.pi*forb))**2)**(1.0/3.0)
+# creates a standard LISA geometry object
 
-montanaamplitude = (2.0*M1*M2/(r*R) * (G/(c*c))**2)
+lisa = EccentricInclined()
 
-# initial phase, inclination, position in the sky, and polarization of the source
+# creates the SimpleBinary object
 
-montanaphi0 = 0.0
-montanainc = 1.53921
+mysystem = PhysicalBinary(freq = 1.0e-3,
+                          phi0 = 0.0,
+                          inc = math.pi/4.2,
+                          amp = (0.5,0.033,0.05),
+                          elat = math.pi/3.0,
+                          elon = 0.0, 
+                          psi = math.pi/10.0)
+               
+# creates a TDIsignal object with lisa and mysystem
+               
+signalTDI = TDIsignal(lisa,mysystem)
 
-montanacolatitude = 1.57
-montanalongitude = 0.0
+stime = 32.0
 
-montanapsi = 0.0
+# creates a TDInoise object with standard proof-mass and shot noise
+# no laser noise since it would not be canceled with first-generation TDI
 
-# translation between Synthetic LISA and LISA Simulator GW source parameters:
-# - the frequency needs to be adjusted for the different definition of year
-#   in the two codes *1.000702365550482
-# - difference in the definition of inclination
-# - Synthetic LISA uses SSB ecliptic latitude rather than colatitude
-# - difference in the definition of polarization
+noiseTDI =  TDInoise(lisa,stime,2.5e-48,
+                          stime,1.8e-37,
+                          stime,0.0)
 
-# by the way, the following is a Python dictionary: a bit like an
-# array, but you can retrieve values by "key" (in this case, parameter
-# name)
+# get one year's worth of samples
 
-mypars = {'frequency': montanafgw,
-          'phase':     montanaphi0,
-          'inc':       math.pi - montanainc,
-          'amplitude': montanaamplitude,
-          'beta':      0.5*math.pi - montanacolatitude,
-          'lambda':    montanalongitude,
-          'psi':       -montanapsi}
+samples = int(2**25/stime)
+patches = 256
+ 
+[alphas,betas,gammas] = transpose(getobsc(samples,stime,[signalTDI.alpha,
+                                                         signalTDI.beta,
+                                                         signalTDI.gamma]))
 
-#####################################
-# creating the LISA and TDI objects #
-#####################################
+[alphan,betan,gamman] = transpose(getobsc(samples,stime,[noiseTDI.alpha,
+                                                         noiseTDI.beta,
+                                                         noiseTDI.gamma]))
 
-# create a SimpleBinary Wave object
+# form the optimal TDI observables
 
-mybinary = SimpleBinary(mypars['frequency'],
-                        mypars['phase'],
-                        mypars['inc'],
-                        mypars['amplitude'],
-                        mypars['beta'],
-                        mypars['lambda'],
-                        mypars['psi'])
+signalA = (gammas - alphas) / math.sqrt(2.0)
+signalE = (alphas - 2.0*betas + gammas) / math.sqrt(6.0)
+signalT = (alphas + betas + gammas) / math.sqrt(3.0)
 
-# these parameters yield the same initial LISA orientation and position
-# as in the LISA Simulator, v. 2.0
+noiseA = (gamman - alphan) / math.sqrt(2.0)
+noiseE = (alphan - 2.0*betan + gamman) / math.sqrt(6.0)
+noiseT = (alphan + betan + gamman) / math.sqrt(3.0)
 
-circularlisa  = CircularRotating(0.0,3.0*math.pi/2.0,-1.0)
-eccentriclisa = EccentricInclined(0.0,3.0*math.pi/2.0,-1.0)
+# compute the optimal signal to noise (for short time series, this is somewhat
+# sensitive to the fluctuations in the noise, which is used to compute S_n)
 
-# TDIsignal and TDInoise structures
+print "S/N = ", math.sqrt(sn(signalA,noiseA,stime,patches)**2 + 
+                          sn(signalE,noiseE,stime,patches)**2 + 
+                          sn(signalT,noiseT,stime,patches)**2), "over T = ", stime * samples, " s."
 
-circularsignal = TDIsignal(circularlisa,mybinary)
-eccentricsignal = TDIsignal(circularlisa,mybinary)
+# output signal+noise time series and spectra to disk
 
-# need to know at what frequency we're sampling TDI not to alias too badly
+outputXML = lisaXML('data/binary',
+                    author='Michele Vallisneri',
+                    comments='EccentricInclined 1st-gen. optimal-TDI observables for 1 mHz binary')
 
-stime = 16
+outputXML.TDIData(transpose([signalA + noiseA,
+                             signalE + noiseE,
+                             signalT + noiseT]),samples,stime,'AOf,EOf,TOf')
 
-# let's forget laser noise here, and pretend X can do a good job at canceling it
+specA = spect(signalA + noiseA,stime,patches)
+specE = spect(signalE + noiseE,stime,patches)
+specT = spect(signalT + noiseT,stime,patches)
 
-circularnoise = TDInoise(circularlisa,stime,2.5e-48,stime,1.8e-37,stime,0*1.1e-26)
+outputXML.TDISpectraSelfDescribed(transpose([specA[1:,0],specA[1:,1],
+                                                         specE[1:,1],
+                                                         specT[1:,1]]),'f,AOf,EOf,TOf',encoding='Text')
 
-################################
-# computing signals and noises #
-################################
+outputXML.close()
 
-# one year of data is approx 2**25 samples; we use getobs to produce the signals
-# and the noises
+# plot the spectra
 
-samples = 2**17  # 2**17 takes 42s on a 1.25GHz G4
-                 # the graph however was done with stime = 16 and samples = 2**21
-                 # and test-montana.py also needs 2**21
-patches = 16
-
-# "getobsc" (as opposed to "getobs") will display completion and speed date
-
-signalXcirc = getobsc(samples,stime,circularsignal.X)
-noiseXcirc  = getobsc(samples,stime,circularnoise.Xm)
-
-# write the binary time series
-
-writebinary('data/tdibinary-X-circ.bin',signalXcirc)
-writebinary('data/tdibinary-X-noise.bin',noiseXcirc)
-
-# compute signal spectrum without windowing or averaging
-
-sspec = spect(signalXcirc,stime,0)
-
-# on the other hand, average the noise profile
-
-nspec = spect(noiseXcirc ,stime,patches)
-
-# interpolate the noise to the frequencies of the signal spectrum
-# (arrayfns is part of Numeric)
-
-ispec = zeros(shape(sspec),typecode='d')
-ispec[:,0] = sspec[:,0]
-
-import arrayfns
-ispec[:,1] = arrayfns.interp(nspec[:,1],nspec[:,0],ispec[:,0])
-
-# the (S/N)^2 is be given by 2T times the integrated ratio
-# of the spectral densities (the factor of 2 because the spectral
-# density is one-sided); notice however that the df is 1/T,
-# so we need only to sum up the array containing the ratio,
-# and multiply by two
-
-sratio = zeros(shape(sspec)[0],typecode='d')
-sratio[1:] = sspec[1:,1] / ispec[1:,1]
-sn2 = 2.0 * sum(sratio[1:])
-
-print "S/N = %f over %f seconds" % (sqrt(sn2),1.0*stime*samples)
-
-# it is hard to plot the signal meaningfully over noise: plotting
-# twice the signal spectral density on top of the noise spectral
-# density would allow us to read (as a ratio between the curves) the
-# (S/N)^2 IN EACH BIN; however, given the LISA amplitude modulation,
-# S/N is spread on a very short frequency scale (a few bins) around
-# the central frequency of the monochromatic signal
-
-writearray('data/tdibinary-X-noise.txt',nspec[1:])
-writearray('data/tdibinary-X-circ.txt', 2.0*sspec[1:])
+try:
+    import pyx    
+    import os
+    
+    os.system('./plotxml.py ' + 'data/binary.xml' + ' ' + 'eps/binary.pdf')
+    print "Spectra plotted in eps/binary.pdf"
+except:
+    print "Could not output PyX graphics"
